020743790bcac60463b95d48997d4461d1f477f0
max07 <max07@users.noreply.github.com>
2014-09-05 23:09:45 +0530

    Update trialET.cpp
    Running :)

--- trialET.cpp
+++ trialET.cpp
@@ -1,102 +1,123 @@
-#include<bits/stdc++.h>
-#include<random>
+#include <bits/stdc++.h>
 #include<chrono>
+#include<random>
 
 #define ui unsigned int
+
 using namespace std;
 
+
 int main()
-    {
-    
-    ui n=100;//,d=5,m,l,s; 
-  //  double epsi;
-    
-   // cout<<"Enter the number of Nodes (n) where n > 49 : "<<endl;
-   cin>>n;
-  //  assert(n>49);
-    
-   // cout<<"Enter the degree of each node (d) where d > 3 : "<<endl;
-  //  cin>>d;
-//    assert(d > 3);
-    
-//    cout<<"Enter the value of epsilon (epsi) where 0 < epsi < 1  : "<<endl;
-   // cin>>epsi;
- //   assert(epsi>0 && epsi<1);
-    
-    
-  //  cout<<"Enter the value to repeat the loop (s) where  s>=48/epsi : "<<endl;
-  //  cin>>s;
-  //  assert(s>=48/epsi);
-    
-    
-  //  cout<<"Enter the no of random walks (m) where  m>=s*12*sqrt(n)/(epsi^2) : "<<endl;
-  //  cin>>m;
- //   assert(m>=(s*12*sqrt(n)/pow(epsi,2)));
-    
- //   cout<<"Enter the length of random walk (l) where  l>=16*(d^2)*ln(n/epsi) : "<<endl;
- //   cin>>l;
- //   assert(l>=(16*pow(d,2)*log(n/epsi)));
-    
-    
+{  
     /**************************************************************/
     /*     Generating Random Graph                                */     
     /*                                                            */      
     /**************************************************************/
-  unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
+    ui n=50;
+    
+    unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
   
   std::default_random_engine generator (seed);
 
   std::uniform_int_distribution<ui> distribution(1,n);
     
     
-      register vector<set <ui> > G(n+1);
+        vector<list <ui> > G(n+1);
     
-    std::pair<std::set<ui>::iterator,bool> newno;
     
-    for(unsigned int i=1;i<=n;++i)
+    for(ui i=1;i<=n;++i)
         {
-           // ui out=5,in=5;
-            ui sizev = G[i].size();
-        
-            while(sizev < 5)
+        ui n1=0,n2=0,n3=0,n4=0,n5=0;
+    while(n1==n2)
         {
-            ui neighbour;
-            neighbour = distribution(generator);
-            if(neighbour != i)
+        while(n2==n3)
+            {
+            while(n3==n4)
                 {
-            
-                if(G[neighbour].size() < 5)
-                {
-                     G[i].insert(neighbour);
-            
-                if(newno.second==true)
-                    { 
-                        G[neighbour].insert(i);
-                        sizev++;
+                while(n4==n5)
+                    {
+                    while(n5==i || n5==0)
+                        {
+                        n5=distribution(generator);
                     }
-                }
-       
+                    n4=distribution(generator);
+                }        
+                n3=distribution(generator);
             }
-    }   
+            n2=distribution(generator);
+        }
+        n1=distribution(generator);
+    }
+           
+            G[i].unique();
+            G[n5].unique();
+            G[n4].unique();
+            G[n3].unique();
+            G[n2].unique();
+            G[n1].unique();
+         
+        
+              if(G[i].size()<10 && G[n5].size()<10)  
+            {
+                G[i].push_back(n5);
+                G[n5].push_back(i);
+            }    
+            
+            G[i].unique();
+            G[n5].unique();
+           
+              if(G[i].size()<10 && G[n4].size()<10)  
+            {
+                G[i].push_back(n4);
+                G[n4].push_back(i);
+            }    
+            
+            G[i].unique();
+            G[n4].unique();
+           
+              if(G[i].size()<10 && G[n3].size()<10)  
+            {
+                G[i].push_back(n3);
+                G[n3].push_back(i);
+            }    
+           
+            G[i].unique();
+            G[n3].unique();
+            
+              if(G[i].size()<10 && G[n2].size()<10)  
+            {
+                G[i].push_back(n2);
+                G[n2].push_back(i);
+            }    
+            
+            G[i].unique();
+            G[n2].unique();
+             
+              if(G[i].size()<10 && G[n1].size()<10)  
+            {
+                G[i].push_back(n1);
+                G[n1].push_back(i);
+            }    
+            
+            G[i].unique();
+            G[n1].unique();
+        
+            if(G[i].size()<5)
+                  i--;
+            
   }   
     for(ui i=1;i<=n;++i)
         {
         cout<<"Node "<<i<<": " ;
-        for(set<ui>::iterator it = G[i].begin(); it != G[i].end();++it)
+        for(auto it = G[i].begin(); it != G[i].end();++it)
             {
                 cout<<" "<<*it;
         }
         cout<<endl;
     }
-
     
-    /**************************************************************/
-    /*     Random Walks & Expansion Tester                        */     
-    /*                                                            */      
-    /**************************************************************/
-
-        /*
     
+        
     distribution.reset();
     
     ui s=50;
@@ -104,33 +125,42 @@ int main()
         {
             ui v;
         v = distribution(generator);
+        
         vector< ui> pairs1;
+        
+       
         
         for(ui m=1; m<=(sqrt(n)); ++m)
             {
                 ui w=v;
-            std::uniform_int_distribution<ui> dVertex (1,5);
+                std::uniform_int_distribution<ui> dVertex (1,10); 
+            
+            
             for(ui l=1; l<= log2(n);++l)
                 {
-                    
-                  std::set<ui>::iterator rnode;
-                  rnode=G[w].begin();    
+                    ui temp=w;
+                
+                    auto it=G[w].begin();    
                     
                     w = dVertex(generator);
-            
-                    while(w--)
-                    {
-                        rnode++;
+                    
+                    if(w<=G[w].size())
+                    { 
+                        while(--w)
+                            it++;
+                        
+                        w=*it;
                     }
                     
-                    w=*rnode;
+                    else
+                        w=temp;
                     
             }  
             
             
             pairs1.push_back(w);
             
-        }
+        } 
             sort(pairs1.begin(),pairs1.end());
                     
             ui collision= pairs1.size();
@@ -144,13 +174,10 @@ int main()
             collision = collision- pairs1.size();
             if(collision < (((sqrt(n))*((sqrt(n))-1))/(2*n)))
                 {cout<<"Reject"<<endl;return 0;}
-                
+              
         
     }
         cout<<"Accept"<<endl;
-    
-    
-    */
-    
-    return 0;
+
+return 0;
 }
